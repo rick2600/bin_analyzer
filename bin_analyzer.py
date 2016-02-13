c65e547fbd9105401d58584dc0da2d8b16cd768f
bernardo.maia <bernardo.maia@corp.globo.com>
2016-02-12 23:44:48 -0200

    Changing arguments to accept standalone files and default parameters

--- bin_analyzer.py
+++ bin_analyzer.py
@@ -7,21 +7,20 @@ import argparse
 
 def parse_args():
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='bin_analyzer: static analysis of ELF binaries')
 
     parser.add_argument('-l', '--list', 
                         action='store_true', help='list modules')
-
-    parser.add_argument('-d', '--dir', 
-                        type=str, help='directory of binaries')
 
     parser.add_argument('-v', '--verbose', 
                         type=int, 
                         default=0,
                         help='control verbosity (1-3)')
 
-    parser.add_argument('-m', '--mods', 
-                        type=str, 
+    parser.add_argument('-m', '--mods',
+                        nargs='?',
+                        type=str,
+                        default='checksec,dangerous,finddeps,sha1sum,strings,tagger,topfuncref',
                         help='run only this list of modules (sep by comma) \
                         <default: all>')
 
@@ -37,29 +36,18 @@ def parse_args():
                         type=str, 
                         help='directory to save output')
 
+    parser.add_argument(dest='file',
+                        type=str, help='standalone binary or directory of binaries')
+
     parser.set_defaults(no_color=False)
 
     return parser.parse_args()
 
-def validate_args(args):
-    valid = True
-    if args.list == False:
-        if args.dir == None:
-            print "-d required"
-            valid = False
-        if args.out_dir == None:
-            print "-o required"
-            valid = False
-
-    return valid
-
-
 def main():
     args = parse_args()
-    if validate_args(args):
-        bin_analyzer = BinAnalyzer(args)
-        bin_analyzer.pre_scan()
-        bin_analyzer.scan()
+    bin_analyzer = BinAnalyzer(args)
+    bin_analyzer.pre_scan()
+    bin_analyzer.scan()
 
 if __name__ == "__main__":
     main()
